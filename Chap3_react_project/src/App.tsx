c16e5d6f0bb76511f7ee0909f3e0e094f0c2c210
komi1230 <komi@ksquad.jp>
2024-11-08 13:41:26 +0900

    fix

---
+++ Chap3_react_project/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return (
+		<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
+			React-v3
+		</header>
+	);
+}
+
+export default App;
